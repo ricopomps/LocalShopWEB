56e0a1805a9f7de90befc8edc91c58d8a06a4893
Ricardo Pompilio <ricardopompiliodossantos@gmail.com>
2023-07-07 17:36:51 -0300

    Refactor: Changed axios config

--- src/network/notes_api.ts
+++ src/network/notes_api.ts
@@ -4,8 +4,12 @@ import axios from "axios";
 const { REACT_APP_API_BASE_URL: baseUrl } = process.env;
 
 export const API = axios.create({
-  baseURL: baseUrl,
   withCredentials: true,
+  baseURL: baseUrl,
+  headers: {
+    "Access-Control-Allow-Origin": "*",
+    "Content-Type": "application/json",
+  },
 });
 
 //USER ROUTES
