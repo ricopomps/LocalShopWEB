8b1cd8b1d9b8182fb2fd42f3cdedc336b3630415
Maria Beatriz <mariabeatriz.lima@ufrpe.br>
2023-08-13 23:49:08 -0300

    Configurando CSS e o CadastroShopperPage.

--- src/pages/CadastroShopperPage.tsx
+++ src/pages/CadastroShopperPage.tsx
@@ -1,20 +1,22 @@
+import styles from "../styles/CadastroShopper.module.css";
+
 const CadastroShopperPage = () => {
-const placeholderLogin = 'Insira seu login...'
-const placeholderEmail= 'Insira seu email...'
-const placeholderSenha = 'Insira sua senha...'
-const placeholderCPF = 'Insira sua cpf...'
+  const placeholderLogin = 'Insira seu login...'
+  const placeholderEmail = 'Insira seu email...'
+  const placeholderSenha = 'Insira sua senha...'
+  const placeholderCPF = 'Insira seu CPF...'
 
   return (
-    <div className="main">
-        {/* <img src={logo} alt='logo' className='imageLogin'/> */}
-        <input type='text' placeholder={placeholderLogin} className='inputLogin'/>
-        <input type='text' placeholder={placeholderEmail} className='inputLogin'/>
-        <input type='text' placeholder={placeholderCPF} className='inputLogin'/>
-        <input type='password' placeholder={placeholderSenha} className='inputSenha'/>
-        <button className='btn btn_cadastro'>CADASTRAR</button>
-        <button className='btn btn_login'>VOLTAR</button>
+    <div className={styles.main}>
+      {/* <img src={logo} alt='logo' className='imageLogin'/> */}
+      <input type='text' placeholder={placeholderLogin} className='inputLogin' />
+      <input type='text' placeholder={placeholderEmail} className='inputLogin' />
+      <input type='text' placeholder={placeholderCPF} className='inputLogin' />
+      <input type='password' placeholder={placeholderSenha} className='inputSenha' />
+      <button className='btn btn_cadastro'>CADASTRAR</button>
+      <button className='btn btn_login'>VOLTAR</button>
 
-     </div>
+    </div>
   );
 };
 
