f38733e23c09d02b09884bf681d195478d77fbc0
Ricardo Pompilio <ricardopompiliodossantos@gmail.com>
2023-08-22 12:39:39 -0300

    feat: implemented support column

--- src/components/Grid.tsx
+++ src/components/Grid.tsx
@@ -1,31 +1,70 @@
 import React, { useState } from "react";
 import styles from "../styles/Grid.module.css";
-import { Col, Row } from "react-bootstrap";
+import { Button, Col, Row } from "react-bootstrap";
 
 interface GridProps {
   rows: number;
   cols: number;
 }
 
-interface SelectedCells {
+interface CellCoordinates {
   x: number;
   y: number;
+  type: string;
 }
 
 const Grid: React.FC<GridProps> = ({ rows, cols }) => {
-  const [selectedCells, setSelectedCells] = useState<SelectedCells[]>([]);
+  const [selectedCells, setSelectedCells] = useState<CellCoordinates[]>([]);
+  const [selectedStyle, setSelectedStyle] = useState<string>("");
   const cellSize = 50;
+  const spacing = 10;
+
+  const supportColumn = [
+    { style: styles.green, text: "Prateleira" },
+    { style: styles.blue, text: "Frios" },
+    { style: styles.purple, text: "Caixa" },
+    { style: styles.red, text: "Obstáculo" },
+  ];
+
+  const handleClick = (x: number, y: number, type: string) => {
+    const cellIsSelected = selectedCells.some(
+      (cell) => cell.x === x && cell.y === y
+    );
+    const newSelectedCells = cellIsSelected
+      ? selectedCells.filter((cell) => cell.x !== x || cell.y !== y)
+      : [...selectedCells, { x, y, type }];
+
+    setSelectedCells(newSelectedCells);
+  };
+
+  const isCellSelected = (x: number, y: number) => {
+    return selectedCells.some((cell) => cell.x === x && cell.y === y);
+  };
+
+  const getCellType = (x: number, y: number) => {
+    const cell = selectedCells.find((cell) => cell.x === x && cell.y === y);
+    return cell?.type;
+  };
+
+  const getCellSyle = (x: number, y: number) => {
+    const type = getCellType(x, y);
+    const selected = supportColumn.find((a) => a.text === type);
+    return selected?.style;
+  };
+
+  const generateCellKey = (x: number, y: number) => {
+    return `cell-${x}-${y}`;
+  };
+
   const gridStyle = {
     gridTemplateRows: `repeat(${rows}, ${cellSize}px)`,
     gridTemplateColumns: `repeat(${cols}, ${cellSize}px)`,
   };
 
-  const handleClick = (x: number, y: number) => {
-    if (selectedCells.find((s) => s.x === x && s.y === y)) {
-      setSelectedCells(selectedCells.filter((s) => s.x !== x || s.y !== y));
-    } else {
-      setSelectedCells([...selectedCells, { x, y }]);
-    }
+  const supportColumnStyle = {
+    gridTemplateRows: `repeat(${rows}, ${cellSize}px)`,
+    gridTemplateColumns: `${rows}, ${cellSize}px`,
+    gap: `${spacing}px`,
   };
 
   return (
@@ -35,22 +74,32 @@ const Grid: React.FC<GridProps> = ({ rows, cols }) => {
           {Array.from({ length: rows * cols }, (_, index) => {
             const x = index % cols;
             const y = Math.floor(index / cols);
-            const isSelected = selectedCells.some(
-              (cell) => cell.x === x && cell.y === y
-            );
 
             return (
               <div
-                key={index}
-                onClick={() => handleClick(x, y)}
+                key={generateCellKey(x, y)}
+                onClick={() => handleClick(x, y, selectedStyle)}
                 className={`${styles.gridCell} ${
-                  isSelected && styles.gridCellGreen
+                  isCellSelected(x, y) && getCellSyle(x, y)
                 }`}
-              >
-                {/* {`${x}-${y}`} */}
-              </div>
+              ></div>
             );
           })}
+        </div>
+      </Col>
+      <Col>
+        <Button onClick={() => setSelectedCells([])}>clear</Button>
+        <Button onClick={() => console.log([selectedCells])}>show</Button>
+        <div className={styles.supportColumn} style={supportColumnStyle}>
+          {supportColumn.map((item, index) => (
+            <div
+              key={`support-cell-${index}`}
+              className={`${styles.gridCell} ${item.style}`}
+              onClick={() => setSelectedStyle(item.text)}
+            >
+              {item.text}
+            </div>
+          ))}
         </div>
       </Col>
     </Row>
