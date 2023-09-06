5a684dc2d5819c0cd529f33fe148a96076ea55e8
Ricardo Pompilio <ricardopompiliodossantos@gmail.com>
2023-09-06 12:14:02 -0300

    style: Change toast color

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,8 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./styles/global.css";
 import App from "./App";
+import "./styles/global.css";
 import reportWebVitals from "./reportWebVitals";
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
